fc85629899733920463849b56c3a7be4411a5b2b
dr.number <dr.number@yandex.ru>
2022-09-11 23:15:42 +0700

    update

--- APIStripe/models.py
+++ APIStripe/models.py
@@ -5,7 +5,6 @@ class Item(models.Model):
     name = models.CharField(max_length=100)
     description = models.CharField(max_length=255)
     price = models.DecimalField(max_digits=19, decimal_places=2)
-    count_in_basked = models.IntegerField(default=0)
 
     def get_price(self):
         return str(self.price)
